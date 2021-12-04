ae71e9372563d8099d4144e80ef98a541f19979d
sonu kumar mahto <sonukumarmahto@sonus-MacBook.local>
2021-12-04 14:14:36 +0530

    olympics query

---
+++ olympics_data_query.sql
@@ -0,0 +1,140 @@
+select * from olympics_history;
+select * from olympics_history_noc_regions;
+-- Q1-How many olympics games have been held?
+select count(distinct games) from olympics_history;
+--Q2.List down all Olympics games held so far.
+select year,season,city from olympics_history;
+
+--Mention the total no of nations who participated in each olympics game?
+
+with country as
+		(select games, nr1.region as country
+		from olympics_history oh1
+		join olympics_history_noc_regions nr1 ON nr1.noc=oh1.noc
+		group by games, nr1.region)
+		  
+select games ,count(1)  as total_countries from country
+group by games order by games;
+
+			
+
+			  
+  
+
+
+
+--Which year saw the highest and lowest no of countries participating in olympics?
+with t1 as
+		(select distinct year,name from olympics_history),
+		t2 as
+		(select distinct year as given_year from t1
+		group by year),
+		t3 as 
+		(select name , count(1) as from t1)
+--Which nation has participated in all of the olympic games?
+with tot_games as
+              (select count(distinct games) as total_games
+              from olympics_history),
+          countries as
+              (select games, nr1.region as country
+              from olympics_history oh1
+              join olympics_history_noc_regions nr1 ON nr1.noc=oh1.noc
+              group by games, nr1.region),
+          countries_participated as
+              (select country, count(1) as total_participated_games
+              from countries
+              group by country)
+			  
+select cpc.* from countries_participated cpc 
+join tot_games t1 on t1.total_games=cpc.total_participated_games;
+--Identify the sport which was played in all summer olympics.
+with t1 as 
+	(select distinct games,sport from olympics_history
+		where season='Summer'),
+	tot_games as
+              (select count(distinct games) as total_games
+              from olympics_history where season='Summer'),
+  	t3 as
+		(select sport,count(1) as total_games
+		from t1 group by sport)
+		
+select * from t3 join tot_games on tot_games.total_games=t3.total_games;
+select sport,count(1) as no_of_games from olympics_history
+where season='Summer' group by sport;
+group by sport order by sport;
+
+select * from olympics_history;
+
+select sport,count(1) from olympics_history
+where season='Summer'
+group by sport;
+--Which Sports were just played only once in the olympics?
+with t1 as
+		(select  distinct games,sport from olympics_history),
+	t2 as
+		(select sport, count(1) as once_played
+		from t1 group by sport)
+select t2.*,t1.games from t1
+join t2 on t1.sport=t2.sport and t2.once_played=1;
+		
+	
+select * from t1 where t1.once_played=1;
+--Fetch the total no of sports played in each olympic games.
+with t1 as
+      	(select distinct games,sport
+      	from olympics_history),
+        t2 as
+      	(select games, count(1) as no_of_sports
+      	from t1
+      	group by games)
+      select * from t2
+      order by no_of_sports desc;
+
+-- Fetch details of the oldest athletes to win a gold medal.
+
+with temp as
+		(select name,sex,cast(case when age='NA' then '0' else age end as int) as age
+		,team,games,city,sport, event, medal
+            from olympics_history),
+		t1 as 
+		(select * ,rank() over(order by age desc) as rnk from temp
+		where medal='Gold')
+		
+select * from t1 where rnk=1;
+
+
+
+
+
+
+-- Find the Ratio of male and female athletes participated in all olympic games.
+with t1 as 
+		(select sex ,count(1) as cnt from olympics_history
+		group by sex),
+	t2 as
+		(select *, row_number() over(order by cnt) as rn
+        	 from t1),
+	 min_cnt as
+        	(select cnt from t2	where rn = 1),
+	max_cnt as
+		(select cnt from t2	where rn = 2)
+select concat('1 : ', round(max_cnt.cnt::decimal/min_cnt.cnt, 2)) as ratio
+    from min_cnt, max_cnt;
+-- Fetch the top 5 athletes who have won the most gold medals.
+with t1 as 
+		(select name,team, count(1) total_gold_medals from olympics_history
+		 where medal='Gold'
+		 group by name,team),
+	 t2 as 
+	 	(select *,dense_rank() over(order by total_gold_medals desc) rnk
+		from t1)
+select name,team,total_gold_medals from t2 where rnk<=5;
+-- Fetch the top 5 athletes who have won the most medals (gold/silver/bronze).
+-- Fetch the top 5 most successful countries in olympics. Success is defined by no of medals won.
+-- List down total gold, silver and broze medals won by each country.
+-- List down total gold, silver and broze medals won by each country corresponding to each olympic games.
+-- Identify which country won the most gold, most silver and most bronze medals in each olympic games.
+-- Identify which country won the most gold, most silver, most bronze medals and the most medals in each olympic games.
+-- Which countries have never won gold medal but have won silver/bronze medals?
+-- In which Sport/event, India has won highest medals.
+-- Break down all olympic games where india won medal for Hockey and how many medals in each olympic games.
